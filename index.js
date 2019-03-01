176a3ab7f21c6904adcbe1ee8ce4ffe4773ee297
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-02-28 19:04:32 -0600

    Added post user and get all users

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 // implement your API here
-//XP
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+const PORT = '3001';
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+
+  if(!name || !bio) {
+    res.status(400).json({errorMessage: 'Please provide a name and bio for the user.'});
+  } else {
+    db.insert({name, bio})
+      .then(users => {
+        res.status(201).json(users)
+      }).catch(err => {
+        res.status(500).json({error: 'There was an error while saving.'})
+      });
+  }
+});
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users)
+    }).catch(err => {
+      res.status(500).json({error: 'The users information could not be retreived!'})
+    });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
